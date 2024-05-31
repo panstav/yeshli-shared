04ff106fbb9f358ce2ec2952021841802600cb5f
Stav Geffen <stavgeffen@gmail.com>
2024-05-31 14:01:37 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+export const roles = {
+	order: ['GUEST', 'TRIAL', 'MEMBER', 'EDITOR', 'ADMIN'],
+	GUEST: 'GUEST',
+	TRIAL: 'TRIAL',
+	MEMBER: 'MEMBER',
+	EDITOR: 'EDITOR',
+	ADMIN: 'ADMIN'
+};
+
+// cspell:ignore alon, elyse
+export const themes = [
+	{
+		themeName: "alon-01",
+		parentDomain: "yesh.li"
+	},
+	{
+		themeName: "elyse-01",
+		parentDomain: "yesh.li"
+	},
+	{
+		themeName: "tom-01",
+		parentDomain: "yesh.li-eng",
+		collectionPages: [
+			{
+				type: "post",
+				prefix: "blog",
+				componentPath: "pages/Post/index.js"
+			},
+			{
+				type: "tag",
+				prefix: "blog/tag",
+				componentPath: "pages/Tag/index.js"
+			}
+		]
+	}
+];
+
+export const misc = {
+	yeshLiHomepagePurchaseId: 'purchase',
+};
---
+++ test.js
@@ -0,0 +1,44 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+import { roles, themes } from './index.js';
+
+describe('roles', () => {
+
+	it('should contain all roles as keys and in order array', () => {
+		roles.order.forEach(role => {
+			assert(roles[role]);
+		});
+
+		Object.keys(roles).forEach(role => {
+			if (role === 'order') return;
+			assert(roles.order.includes(role));
+		});
+	});
+
+});
+
+describe('themes', () => {
+
+	it('should have all themes contain all required fields', () => {
+		const requiredFields = ['themeName', 'parentDomain'];
+		themes.forEach(theme => {
+			requiredFields.forEach(field => {
+				assert(theme[field]);
+			});
+		});
+	});
+
+	it('should have all themes with collection pages to have all required fields', () => {
+		const requiredFields = ['type', 'prefix', 'componentPath'];
+		themes.forEach(theme => {
+			if (!theme.collectionPages) return;
+			theme.collectionPages.forEach(collectionPage => {
+				requiredFields.forEach(field => {
+					assert(collectionPage[field]);
+				});
+			});
+		});
+	});
+
+});
