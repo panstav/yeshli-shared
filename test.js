796f49e54166ec6bf6e6172cca37882d9b55c525
Stav Geffen <stavgeffen@gmail.com>
2024-06-04 16:59:25 +0545

    feat(es5-dist)

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
 
-import { roles, themes } from './index.js';
+import { roles, themes } from './src/index.js';
 
 describe('roles', () => {
 
